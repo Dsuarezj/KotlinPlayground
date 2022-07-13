cda60eee477d1c41178fa3d9c8523028318251a2
Dsuarezj <dsuarez@thoughtworks.com>
2022-07-13 21:15:15 +0200

    Object class for singleton pattern

---
+++ src/main/kotlin/com/kotlin/playground/classes/Authenticate.kt
@@ -0,0 +1,12 @@
+package com.kotlin.playground.classes
+
+// This is helpful when trying to implement Singleton pattern, so you don't have to create a instance of the class everytime, you just called it
+object Authenticate {
+    fun authenticate(userName: String, password: String){
+        println("Authenticating user $userName")
+    }
+}
+
+fun main() {
+    Authenticate.authenticate("usuario", "pass")
+}
