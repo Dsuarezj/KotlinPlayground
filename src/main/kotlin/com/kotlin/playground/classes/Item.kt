1a34082e45348fde5bf535edffda61dc79d61ab2
Dsuarezj <dsuarez@thoughtworks.com>
2022-07-13 20:03:36 +0200

    Getters and setters

--- src/main/kotlin/com/kotlin/playground/classes/Item.kt
+++ src/main/kotlin/com/kotlin/playground/classes/Item.kt
@@ -1,7 +1,18 @@
 package com.kotlin.playground.classes
 
+import java.lang.IllegalArgumentException
+
 class Item() {
     var name: String = ""
+    var price: Double = 0.0
+        // get() = field // this is define as default no need to declare it again only if you want to add custom logic
+        get() {
+            println("Do something else before return")
+            return field
+        }
+        set(value) {
+            if(value >= 0.0) field = value else throw IllegalArgumentException("no negative prices")
+        }
 
     constructor(_name: String): this() {
         name = _name
@@ -12,5 +23,8 @@ fun main(){
     val item = Item("cosa")
     println("Item name ${item.name}")
     item.name = "otra cosa"
-    println("Item name ${item.name}")
+    item.price = 10.0
+    println("Item name ${item.name} with price ${item.price}")
+    item.price = -1.0
+
 }
