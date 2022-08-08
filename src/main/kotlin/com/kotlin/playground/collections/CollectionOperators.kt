f8d10137476ba0fa9a049280bde340795429156c
Dsuarezj <dsuarez@thoughtworks.com>
2022-08-08 16:08:59 +0200

    map operations

--- src/main/kotlin/com/kotlin/playground/collections/CollectionOperators.kt
+++ src/main/kotlin/com/kotlin/playground/collections/CollectionOperators.kt
@@ -14,6 +14,21 @@ fun main() {
             c: Course -> c.category == CourseCategory.DESIGN
     }
     exploreFilter(courseList, designPredicate)
+
+    exploreMap(courseList,designPredicate)
+}
+
+fun exploreMap(courseList: MutableList<Course>, predicate: (Course) -> Boolean) {
+    val courses = courseList
+        .filter(predicate) // in here we are not using the lambda sintaxis {} but calling filter function () and passing the predicate
+        .map {
+            "${it.name} - ${it.category}"
+        }
+        .forEach{
+            println("courses names: $it")
+        }
+
+    println("courses after foreach: $courses") // this won't print anything since foreach does not return anything
 }
 
 fun exploreFilter(courseList: MutableList<Course>, predicate: (Course) -> Boolean) {
