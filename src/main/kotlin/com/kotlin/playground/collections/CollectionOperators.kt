a3bae39ef25730590613d1be69315d7505413efd
Dsuarezj <dsuarez@thoughtworks.com>
2022-08-09 13:35:57 +0200

    Revert "Move kotlin course to another source and add dependecies"
    This reverts commit d84f2c5846679a5ca7626abd285be80fe0138043.

---
+++ src/main/kotlin/com/kotlin/playground/collections/CollectionOperators.kt
@@ -0,0 +1,104 @@
+package com.kotlin.playground.collections
+
+import com.kotlin.playground.dataset.Course
+import com.kotlin.playground.dataset.CourseCategory
+import com.kotlin.playground.dataset.KAFKA
+import com.kotlin.playground.dataset.courseList
+
+fun main() {
+    val courseList = courseList()
+    val devPredicate = {
+        c: Course -> c.category == CourseCategory.DEVELOPEMENT
+    }
+    exploreFilter(courseList, devPredicate)
+    val designPredicate = {
+            c: Course -> c.category == CourseCategory.DESIGN
+    }
+    exploreFilter(courseList, designPredicate)
+    exploreMap(courseList,designPredicate)
+    val list = listOf(listOf(1,2,3), listOf(4,5,6))
+    val mapResult = list.map { innerList -> innerList.map {
+        it.toDouble()
+    } }
+
+    println("map Result: $mapResult")
+
+    val flatMapResult = list.flatMap { innerList -> innerList.map {
+        it.toDouble()
+    } }
+
+    println("flatmap Result: $flatMapResult")
+
+    val course = exploreFlatMap(courseList, KAFKA)
+    println("courses filter: $course")
+
+    exploreHashMap()
+    collectionNullability()
+}
+
+fun collectionNullability() {
+    var list: MutableList<String>? = null
+    list = mutableListOf()
+    list.add("Alice")
+    list.forEach {
+        println("value of nullable list: $it")
+    }
+
+    val listWithNullElements: List<String?> = listOf("Alice", null, "Bob")
+    listWithNullElements.forEach{
+        println("Length is: ${it?.length}")
+    }
+}
+
+fun exploreHashMap() {
+    val nameAge = mutableMapOf("dilip" to 33, "alice"  to 23)
+    nameAge.forEach{
+        (k,v) ->
+        println("key: $k, value: $v")
+    }
+    println(nameAge["alice"])
+    nameAge.getOrElse("Bob") {"default"}
+    println(nameAge.containsKey("abc"))
+
+    val filterMap = nameAge.filterKeys { it.endsWith("e") }
+        .map { it.key.uppercase() }
+
+    println("Filter hash map $filterMap")
+
+    val oldest = nameAge.maxByOrNull { it.value }
+    println("Oldest age $oldest")
+
+}
+
+fun exploreFlatMap(courseList: MutableList<Course>, topic: String): List<String> {
+    return courseList.flatMap { course ->
+        val courseName = course.name
+        course.topicsCovered.filter {
+            it == topic
+        }.map {
+            println("inside map, inside flatmap: $it")
+            courseName
+        }
+    }
+}
+
+fun exploreMap(courseList: MutableList<Course>, predicate: (Course) -> Boolean) {
+    val courses = courseList
+        .filter(predicate) // in here we are not using the lambda sintaxis {} but calling filter function () and passing the predicate
+        .map {
+            "${it.name} - ${it.category}"
+        }
+        .forEach{
+            println("courses names: $it")
+        }
+
+    println("courses after foreach: $courses") // this won't print anything since foreach does not return anything
+}
+
+fun exploreFilter(courseList: MutableList<Course>, predicate: (Course) -> Boolean) {
+    val developmentCourse = courseList
+        .filter { predicate.invoke(it) }
+        .forEach{
+            println("filtered courses: $it")
+        }
+}
---
+++ src/main/kotlin/com/kotlin/playground/exceptions/ExploreExceptions.kt
@@ -0,0 +1,21 @@
+package com.kotlin.playground.exceptions
+
+fun main() {
+    println("Name length is ${nameLength("Alicce")}")
+    println("Name length is ${nameLength(null)}")
+    returnNothing()
+
+}
+
+fun nameLength(name: String?): Int? {
+    return try {
+         name!!.length
+     } catch (ex: Exception) {
+         println("Exception is: $ex")
+         null
+     }
+}
+
+fun returnNothing(): Nothing {
+    throw RuntimeException("usless")
+}
